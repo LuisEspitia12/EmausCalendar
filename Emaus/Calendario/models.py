9569f368428d2b41c712f6daeed9f9a2b5da89ec
LuisEspitia12 <luis.espitia.rodriguez@gmail.com>
2024-05-29 07:37:40 -0500

    Start the new project
    Create Emaus project and Calendario app also Create the models "pais", "ciudad", "Parroquia", "coordinador" and "Grupo"

---
+++ Emaus/Calendario/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.pais}"
+
+class Parroquia(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+class Coordinador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Asume que usas el sistema de usuarios de Django
+    parroquia = models.ForeignKey(Parroquia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=255)
+    numero_de_retiro = models.IntegerField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    parroquia = models.ForeignKey(Parroquia, on_delete=models.CASCADE)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    coordinador = models.ForeignKey(Coordinador, on_delete=models.CASCADE)
+    telefono_inscripcion_whatsapp = models.CharField(max_length=20)
+    formulario_inscripcion = models.URLField()
+    correo_electronico = models.EmailField()
+    casa_de_retiro = models.CharField(max_length=255)
+    poster = models.ImageField(upload_to='/Emaus/Calendario/static/img/posters')
+
+    def __str__(self):
+        return self.nombre
+
+
